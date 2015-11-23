22d1d803e2b00347d83e20e00b1cefe5255b1f8f
gwilym <gwilym@gwilymh.com>
2015-11-23 16:51:46 +1100

    rm logging

--- src/middleware/internal.js
+++ src/middleware/internal.js
@@ -1,7 +1,6 @@
 
 export default function createInternalAuth(options) {
   const {secret} = options
-  console.log('making auth', options)
   return (req, res, next) => {
     if (req.query.$auth_secret === secret) req.user = {dummy: true, superuser: true, auth_by_secret: secret}
     next()
