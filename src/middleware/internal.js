c320cf1885bcb60946d0865d26bf6e151495a0dd
gwilym <gwilym@gwilymh.com>
2016-01-05 16:20:26 +1100

    logging

--- src/middleware/internal.js
+++ src/middleware/internal.js
@@ -5,17 +5,13 @@ export default function createInternalAuth(options) {
     if (req.query.$auth_secret === secret) {
       delete req.query.$auth_secret
       if (User && req.query.$user_id) {
-        console.log('req.query', req.query)
         let user_id
-        console.log('req.query.$user_id before parse', req.query.$user_id)
         try {
           user_id = JSON.parse(req.query.$user_id)
         }
         catch (err) {
           user_id = req.query.$user_id
-          console.log('err', user_id)
         }
-        console.log('user_id after parse', user_id)
         return User.findOne(user_id, (err, user) => {
           if (err) return res.status(500).send(`[fl-auth] createInternalAuth: Error retrieving $user_id ${req.query.$user_id} ${err.message || err}`)
           if (!user) return res.status(500).send(`[fl-auth] createInternalAuth: Can't find $user_id ${req.query.$user_id}`)
