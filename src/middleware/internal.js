98774bbf1c7a26801202e5b7148e7bf84cd5dba9
gwilym <gwilym@gwilymh.com>
2016-05-26 09:07:12 +1000

    internal auth fixes

--- src/middleware/internal.js
+++ src/middleware/internal.js
@@ -1,29 +1,42 @@
 
-export default function createInternalAuth(options) {
-  const {User, secret} = options
+const noop = (req, res, next) => next()
+
+
+export default function createInternalMiddleware(options) {
+  const {User, secret, deserializeUser} = options
+
+  if (!(User || deserializeUser)) {
+    console.error('[fl-auth] createInternalMiddleware requires a User or deserializeUser option')
+    return noop
+  }
+
+  const getUser = deserializeUser || ((userId, callback) => User.findOne(userId, callback))
+
   return (req, res, next) => {
-    if (req.query.$auth_secret === secret) {
-      delete req.query.$auth_secret
-      if (User && req.query.$user_id) {
-        let userId
-        try {
-          userId = JSON.parse(req.query.$user_id)
-        }
-        catch (err) {
-          userId = req.query.$user_id
-        }
-        return User.findOne(userId, (err, user) => {
-          if (err) return res.status(500).send(`[fl-auth] createInternalAuth: Error retrieving $user_id ${req.query.$user_id} ${err.message || err}`)
-          if (!user) return res.status(500).send(`[fl-auth] createInternalAuth: Can't find $user_id ${req.query.$user_id}`)
-          req.user = user
-          delete req.query.$user_id
-          next()
-        })
-      }
-      req.user = {id: 'dummy', dummy: true, authBySecret: secret}
-      req.user.get = prop => req.user[prop]
-      return next()
+
+    if (!req.query.$auth_secret) return noop
+    if (req.query.$auth_secret !== secret) {
+      console.error('[fl-auth] createInternalMiddleware: Non-matching $auth_secret supplied on query - ', req.query.$auth_secret)
+      return noop
     }
-    next()
+    delete req.query.$auth_secret
+
+    if (!req.query.$user_id) return noop
+
+    let userId
+    try {
+      userId = JSON.parse(req.query.$user_id)
+    }
+    catch (err) {
+      userId = req.query.$user_id
+    }
+
+    getUser(userId, (err, user) => {
+      if (err) return res.status(500).send(`[fl-auth] createInternalMiddleware: Error retrieving $user_id ${req.query.$user_id} ${err.message || err}`)
+      if (!user) return res.status(500).send(`[fl-auth] createInternalMiddleware: Can't find $user_id ${req.query.$user_id}`)
+      req.user = user
+      delete req.query.$user_id
+      next()
+    })
   }
 }
